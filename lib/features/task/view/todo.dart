835a697cb7efa53506bfb252be0a2d4b4553ad10
Rohan Sunar <rohanksunar15@gmail.com>
2025-06-07 14:59:26 +0530

    set up initial project folder structure

---
+++ lib/features/task/view/todo.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Todo extends StatefulWidget {
+  const Todo({super.key});
+
+  @override
+  State<Todo> createState() => _TodoState();
+}
+
+class _TodoState extends State<Todo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('Todo App')),
+      ),
+      body: Column(
+      ),
+    );
+  }
+}
